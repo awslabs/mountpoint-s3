65e93f539ffa9406aef3cbe8bd728c3e7ba3cc6f
Andreas Neuhaus <zargony@zargony.com>
2013-10-07 16:15:28 +0200

    Added uuid and github url to crate

--- lib.rs
+++ lib.rs
@@ -1,9 +1,12 @@
-#[comment = "Rust FUSE bindings"];
-#[author = "Andreas Neuhaus <zargony@zargony.com>"];
-#[license = "MIT"];
-
+#[link(name = "fuse",
+       vers = "0.1",
+       uuid = "9385b964-5831-426e-b8b1-97acffc564d9",
+       url = "https://github.com/zargony/rust-fuse.git")];
 #[crate_type = "lib"];
-#[link(name = "fuse", vers = "0.1")];
+
+#[comment = "Rust FUSE bindings"];
+#[author = "Andreas Neuhaus <info@zargony.com>"];
+#[license = "MIT"];
 
 pub mod fuse;
 
