7e23e740574b5e0b3766b606e48a353de883ad6d
Josh Stone <cuviper@gmail.com>
2014-10-04 14:42:55 -0700

    Build examples with cargo, and update their deprecations

--- examples/hello.rs
+++ examples/hello.rs
@@ -69,7 +69,7 @@ impl Filesystem for HelloFS {
 
 	fn read (&mut self, _req: &Request, ino: u64, _fh: u64, offset: u64, _size: uint, reply: ReplyData) {
 		if ino == 2 {
-			reply.data(HELLO_TXT_CONTENT.as_bytes().tailn(offset as uint));
+			reply.data(HELLO_TXT_CONTENT.as_bytes().slice_from(offset as uint));
 		} else {
 			reply.error(ENOENT);
 		}
@@ -90,6 +90,6 @@ impl Filesystem for HelloFS {
 }
 
 fn main () {
-	let mountpoint = Path::new(os::args().get(1).as_slice());
+	let mountpoint = Path::new(os::args()[1].as_slice());
 	fuse::mount(HelloFS, &mountpoint, []);
 }
