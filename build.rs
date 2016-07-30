c6611ff85ead4b8de9eaa17b0e3622a17e4cb5c5
Andreas Neuhaus <zargony@zargony.com>
2016-07-30 23:52:02 +0200

    Simplify build.rs by only probing for "fuse" (OSXFUSE also provides fuse.pc)

--- build.rs
+++ build.rs
@@ -1,17 +1,5 @@
 extern crate pkg_config;
 
-use std::env;
-
 fn main () {
-    let target = env::var("TARGET").unwrap();
-    if target.ends_with("-apple-darwin") {
-        // Use libosxfuse on OS X
-        pkg_config::find_library("osxfuse").unwrap();
-    } else if target.ends_with("-unknown-linux-gnu") || target.ends_with("-unknown-freebsd") {
-        // Use libfuse on Linux and FreeBSD
-        pkg_config::find_library("fuse").unwrap();
-    } else {
-        // Fail on unsupported platforms (e.g. Windows)
-        panic!("Unsupported target platform");
-    }
+    pkg_config::probe_library("fuse").unwrap();
 }
