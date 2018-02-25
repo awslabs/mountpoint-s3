e320b6eff05455ef553041e7944d963f94009e44
Jack O'Connor <oconnor663@gmail.com>
2018-02-25 13:23:22 -0500

    upgrade the env_logger dependency
    The latest version no longer returns a result from init(), so the
    examples needed to be tweaked.

--- examples/null.rs
+++ examples/null.rs
@@ -9,7 +9,7 @@ struct NullFS;
 impl Filesystem for NullFS {}
 
 fn main() {
-    env_logger::init().unwrap();
+    env_logger::init();
     let mountpoint = env::args_os().nth(1).unwrap();
     fuse::mount(NullFS, &mountpoint, &[]).unwrap();
 }
