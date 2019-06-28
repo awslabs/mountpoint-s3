f834fbbd5614976e8d480da249d487609504ee6e
Andreas Neuhaus <zargony@zargony.com>
2019-06-28 12:37:11 +0200

    Clean up fuse-sys build.rs and output errors on stderr

--- fuse-sys/build.rs
+++ fuse-sys/build.rs
@@ -1,9 +1,13 @@
 #[cfg(not(target_os = "macos"))]
-static LIBFUSE_NAME: &str = "fuse";
+const LIBFUSE_NAME: &str = "fuse";
 
 #[cfg(target_os = "macos")]
-static LIBFUSE_NAME: &str = "osxfuse";
+const LIBFUSE_NAME: &str = "osxfuse";
 
-fn main () {
-    pkg_config::Config::new().atleast_version("2.6.0").probe(LIBFUSE_NAME).unwrap();
+fn main() {
+    pkg_config::Config::new()
+        .atleast_version("2.6.0")
+        .probe(LIBFUSE_NAME)
+        .map_err(|e| eprintln!("{}", e))
+        .unwrap();
 }
