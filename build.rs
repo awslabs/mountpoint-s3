a0c537c9e3e0643ee12ba3d250b1843404d8c425
Christopher Berner <christopherberner@gmail.com>
2020-10-03 11:29:11 -0700

    Fix build.rs for ABI 7.27, 7.28, 7.30

--- build.rs
+++ build.rs
@@ -12,9 +12,9 @@ const REQUIRED_VERSION: &str = "2.6.0";
 const REQUIRED_VERSION: &str = "2.9.1";
 #[cfg(all(feature = "abi-7-20", not(feature = "abi-7-27")))]
 const REQUIRED_VERSION: &str = "3.0.0";
-#[cfg(all(feature = "abi-7-29", not(feature = "abi-7-30")))]
+#[cfg(all(feature = "abi-7-27", not(feature = "abi-7-30")))]
 const REQUIRED_VERSION: &str = "3.5.0";
-#[cfg(feature = "abi-7-31")]
+#[cfg(feature = "abi-7-30")]
 const REQUIRED_VERSION: &str = "3.9.0";
 
 fn main() {
