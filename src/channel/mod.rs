dc2c89494ee86673d31240f78e939d4d3a9d4565
William Manley <will@stb-tester.com>
2021-03-11 16:51:55 +0000

    test: mount_unmount: Avoid deadlock on test or unmount failure
    If the test fails the `TempDir` drop impl will try to delete the
    mountpoint, but if the filesystem is still mounted this will deadlock as
    it's not backed with any real FUSE filesystem.  Instead we leak because
    the alternative is worse.

--- src/channel/mod.rs
+++ src/channel/mod.rs
@@ -195,7 +195,10 @@ fn is_mounted(fuse_device: &File) -> bool {
 #[cfg(test)]
 mod test {
     use super::*;
-    use std::ffi::{CStr, OsStr};
+    use std::{
+        ffi::{CStr, OsStr},
+        mem::ManuallyDrop,
+    };
 
     #[test]
     fn fuse_args() {
@@ -231,7 +234,10 @@ mod test {
 
     #[test]
     fn mount_unmount() {
-        let tmp = tempfile::tempdir().unwrap();
+        // We use ManuallyDrop here to leak the directory on test failure.  We don't
+        // want to try and clean up the directory if it's a mountpoint otherwise we'll
+        // deadlock.
+        let tmp = ManuallyDrop::new(tempfile::tempdir().unwrap());
         let (file, mount) = Mount::new(&tmp.path(), &[]).unwrap();
         let mnt = cmd_mount();
         eprintln!("Our mountpoint: {:?}\nfuse mounts:\n{}", tmp.path(), mnt,);
@@ -240,7 +246,15 @@ mod test {
         drop(mount);
         let mnt = cmd_mount();
         eprintln!("Our mountpoint: {:?}\nfuse mounts:\n{}", tmp.path(), mnt,);
-        assert!(!mnt.contains(&*tmp.path().to_string_lossy()));
+
+        let detached = !mnt.contains(&*tmp.path().to_string_lossy());
+        assert!(detached);
+
+        if detached {
+            // We've detached successfully, it's safe to clean up:
+            std::mem::ManuallyDrop::<_>::into_inner(tmp);
+        }
+
         // Filesystem may have been lazy unmounted, so we can't assert this:
         // assert!(!is_mounted(&file));
     }
