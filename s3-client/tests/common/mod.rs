27b2d3cb9431a7ee21b2680cb9bf7cf7dc2d496a
Luke Nelson <lukernel@amazon.com>
2022-09-06 20:48:23 +0000

    Import aws Rust sdk to use for testing

--- s3-client/tests/common/mod.rs
+++ s3-client/tests/common/mod.rs
@@ -1,3 +1,6 @@
+use aws_sdk_s3 as s3;
+use bytes::Bytes;
+use s3::Region;
 use s3_client::S3Client;
 
 pub fn get_test_client() -> S3Client {
@@ -6,4 +9,38 @@ pub fn get_test_client() -> S3Client {
 
 pub fn get_test_bucket_name() -> String {
     std::env::var("S3_BUCKET_NAME").expect("Set S3_BUCKET_NAME to run integration tests")
+}
+
+pub fn get_bucket_test_prefix() -> String {
+    std::env::var("S3_BUCKET_TEST_PREFIX").expect("Set S3_BUCKET_NAME to run integration tests")
+}
+
+pub fn get_test_region() -> String {
+    std::env::var("S3_REGION").expect("Set S3_REGION to run integration tests")
+}
+
+pub async fn get_test_sdk_client() -> s3::Client {
+    let config = aws_config::from_env()
+        .region(Region::new(get_test_region()))
+        .load()
+        .await;
+    s3::Client::new(&config)
+}
+
+#[tokio::test]
+async fn test_sdk_create_object() {
+    let sdk_client = get_test_sdk_client().await;
+    let bucket = get_test_bucket_name();
+    let prefix = get_bucket_test_prefix();
+
+    let response = sdk_client
+        .put_object()
+        .bucket(bucket)
+        .key(format!("{}/hello", prefix))
+        .body(s3::types::ByteStream::from(Bytes::from_static(b"Hello")))
+        .send()
+        .await
+        .unwrap();
+
+    println!("{:?}", response);
 }
--- s3-client/tests/list_objects.rs
+++ s3-client/tests/list_objects.rs
@@ -7,14 +7,21 @@ use s3_client::S3Client;
 async fn test_list_objects() {
     tracing_subscriber::fmt::init();
 
+    let sdk_client = get_test_sdk_client().await;
+    let bucket = get_test_bucket_name();
+    let prefix = get_bucket_test_prefix();
+
+    // Create one object named "hello"
+    let key = format!("{}/test_list_objects/hello", prefix);
+    sdk_client.put_object().bucket(&bucket).key(&key).send().await.unwrap();
+
     let client: S3Client = get_test_client();
 
-    let bucket = get_test_bucket_name();
-
     let result = client
-        .list_objects_v2(&bucket, "", "/", None)
+        .list_objects_v2(&bucket, &format!("{}/test_list_objects/", prefix), "/", None)
         .await
         .expect("list_objects_v2 failed");
 
+    assert_eq!(result.objects.len(), 2);
     println!("{:?}", result);
 }
