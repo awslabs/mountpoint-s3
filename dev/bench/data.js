1a3ced81db7399ffdb4d6e6ef6841a8608abb592
github-action-benchmark <github@users.noreply.github.com>
2022-11-01 14:15:49 +0000

    add Benchmark (cargo) benchmark result for 2ec3a27bed7883de488373caa0069f65811768a6

---
+++ dev/bench/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1667312149319,
+  "repoUrl": "https://github.com/awslabs/s3-file-connector",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "monthonk@amazon.co.uk",
+            "name": "Monthon Klongklaew",
+            "username": "monthonk"
+          },
+          "committer": {
+            "email": "monthonk@amazon.co.uk",
+            "name": "Monthon Klongklaew",
+            "username": "monthonk"
+          },
+          "distinct": true,
+          "id": "2ec3a27bed7883de488373caa0069f65811768a6",
+          "message": "Fix missing permission",
+          "timestamp": "2022-11-01T13:58:58Z",
+          "tree_id": "9c0ce11ac9f94422402e27d0b3a562e5f30decbb",
+          "url": "https://github.com/awslabs/s3-file-connector/commit/2ec3a27bed7883de488373caa0069f65811768a6"
+        },
+        "date": 1667312137320,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "event_loop_future",
+            "value": 72554,
+            "range": "± 12762",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_file_benchmark/read_file",
+            "value": 9299553519,
+            "range": "± 1949379157",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_file_benchmark/read_file_direct_io",
+            "value": 8188106273,
+            "range": "± 1372572382",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
