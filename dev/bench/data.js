cd273d2db12d8a295b0a8ce3e6846e6ed6327835
github-action-benchmark <github@users.noreply.github.com>
2022-11-01 14:34:40 +0000

    add Benchmark (cargo) benchmark result for 2ec3a27bed7883de488373caa0069f65811768a6

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667312149319,
+  "lastUpdate": 1667313280850,
   "repoUrl": "https://github.com/awslabs/s3-file-connector",
   "entries": {
     "Benchmark": [
@@ -41,6 +41,48 @@ window.BENCHMARK_DATA = {
             "name": "read_file_benchmark/read_file_direct_io",
             "value": 8188106273,
             "range": "± 1372572382",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "monthonk@amazon.co.uk",
+            "name": "Monthon Klongklaew",
+            "username": "monthonk"
+          },
+          "committer": {
+            "email": "monthonk@amazon.co.uk",
+            "name": "Monthon Klongklaew",
+            "username": "monthonk"
+          },
+          "distinct": false,
+          "id": "2ec3a27bed7883de488373caa0069f65811768a6",
+          "message": "Fix missing permission",
+          "timestamp": "2022-11-01T13:58:58Z",
+          "tree_id": "9c0ce11ac9f94422402e27d0b3a562e5f30decbb",
+          "url": "https://github.com/awslabs/s3-file-connector/commit/2ec3a27bed7883de488373caa0069f65811768a6"
+        },
+        "date": 1667313274107,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "event_loop_future",
+            "value": 63426,
+            "range": "± 4050",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_file_benchmark/read_file",
+            "value": 2575355454,
+            "range": "± 152183174",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_file_benchmark/read_file_direct_io",
+            "value": 2297846188,
+            "range": "± 113849772",
             "unit": "ns/iter"
           }
         ]
