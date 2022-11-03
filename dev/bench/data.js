3d03b842fade7942c1360c0b5ceb6420b3607e2a
github-action-benchmark <github@users.noreply.github.com>
2022-11-03 21:43:43 +0000

    add Benchmark (cargo) benchmark result for f59199b27f0419bcd2ded0d21da9a9f7ce6db02a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1667397045999,
+  "lastUpdate": 1667511823022,
   "repoUrl": "https://github.com/awslabs/s3-file-connector",
   "entries": {
     "Benchmark": [
@@ -125,6 +125,48 @@ window.BENCHMARK_DATA = {
             "name": "read_file_benchmark/read_file_direct_io",
             "value": 918108402,
             "range": "± 3231499576",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "lukernel@amazon.com",
+            "name": "Luke Nelson",
+            "username": "lukenels"
+          },
+          "committer": {
+            "email": "lukernel@amazon.com",
+            "name": "Luke Nelson",
+            "username": "lukenels"
+          },
+          "distinct": true,
+          "id": "f59199b27f0419bcd2ded0d21da9a9f7ce6db02a",
+          "message": "Add AsyncInputStream to connect async Read into CRT InputStream",
+          "timestamp": "2022-11-03T14:30:42-07:00",
+          "tree_id": "b4d01e126f6b03fdca5cd0775cc051945ba2b1cf",
+          "url": "https://github.com/awslabs/s3-file-connector/commit/f59199b27f0419bcd2ded0d21da9a9f7ce6db02a"
+        },
+        "date": 1667511819471,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "event_loop_future",
+            "value": 59205,
+            "range": "± 4218",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_file_benchmark/read_file",
+            "value": 2674035045,
+            "range": "± 238762162",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "read_file_benchmark/read_file_direct_io",
+            "value": 2313406390,
+            "range": "± 72350376",
             "unit": "ns/iter"
           }
         ]
