5c17c750524fe3995c4a09363187e2e477ae502f
Alessandro Passaro <alexpax@amazon.co.uk>
2023-03-31 14:21:21 +0100

    Add tests to document Prefix validation behavior
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3/src/prefix.rs
+++ mountpoint-s3/src/prefix.rs
@@ -38,3 +38,28 @@ impl FromStr for Prefix {
         Prefix::new(s)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use test_case::test_case;
+
+    use super::*;
+
+    #[test_case(" "; "whitespace")]
+    #[test_case("hello"; "not ending in slash")]
+    #[test_case("hello/world"; "nested folder not ending in slash")]
+    fn test_invalid_prefix(prefix: &str) {
+        assert!(!Prefix::is_valid(prefix))
+    }
+
+    #[test_case(""; "empty string")]
+    #[test_case("hello/"; "ending in slash")]
+    #[test_case("hello/world/"; "nested folder ending in slash")]
+    #[test_case(" /"; "whitespace ending in slash")]
+    #[test_case("/"; "single slash")]
+    #[test_case("//"; "double slash")]
+    #[test_case("/hello/"; "starting with slash")]
+    fn test_valid_prefix(prefix: &str) {
+        assert!(Prefix::is_valid(prefix))
+    }
+}
