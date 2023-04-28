0329646254f252c2b1da508a214df3e249c2908f
James Bornholt <bornholt@amazon.com>
2023-04-28 12:45:35 -0500

    Add range argument to client download example (#227)
    This was helpful for debugging #218 and #226.
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>

--- mountpoint-s3-client/examples/download.rs
+++ mountpoint-s3-client/examples/download.rs
@@ -5,6 +5,7 @@ use clap::{Arg, Command};
 use futures::StreamExt;
 use mountpoint_s3_client::{ObjectClient, S3CrtClient};
 use mountpoint_s3_crt::common::rust_log_adapter::RustLogAdapter;
+use regex::Regex;
 use tracing_subscriber::fmt::Subscriber;
 use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
@@ -29,11 +30,26 @@ fn main() {
         .arg(Arg::new("bucket").required(true))
         .arg(Arg::new("key").required(true))
         .arg(Arg::new("region").long("region").default_value("us-east-1"))
+        .arg(
+            Arg::new("range")
+                .long("range")
+                .help("byte range to download (inclusive)")
+                .takes_value(true)
+                .value_name("0-10"),
+        )
         .get_matches();
 
     let bucket = matches.get_one::<String>("bucket").unwrap();
     let key = matches.get_one::<String>("key").unwrap();
     let region = matches.get_one::<String>("region").unwrap();
+    let range = matches.get_one::<String>("range").map(|s| {
+        let range_regex = Regex::new(r"^(?P<start>[0-9]+)-(?P<end>[0-9]+)$").unwrap();
+        let matches = range_regex.captures(s).expect("invalid range");
+        let start = matches.name("start").unwrap().as_str().parse::<u64>().unwrap();
+        let end = matches.name("end").unwrap().as_str().parse::<u64>().unwrap();
+        // bytes range is inclusive, but the `Range` type is exclusive, so bump the end by 1
+        start..(end + 1)
+    });
 
     let client = S3CrtClient::new(region, Default::default()).expect("couldn't create client");
 
@@ -41,7 +57,7 @@ fn main() {
     let last_offset_clone = Arc::clone(&last_offset);
     futures::executor::block_on(async move {
         let mut request = client
-            .get_object(bucket, key, None, None)
+            .get_object(bucket, key, range, None)
             .await
             .expect("couldn't create get request");
         loop {
