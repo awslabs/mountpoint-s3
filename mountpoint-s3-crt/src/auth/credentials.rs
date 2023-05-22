91df4c441b507aefc3c8421d13881589378fa740
James Bornholt <bornholt@amazon.com>
2023-05-22 12:03:38 -0500

    Allow custom credentials providers for S3CrtClient (#252)
    * Allow custom credentials providers for S3CrtClient
    
    This slightly generalizes the work we've done to expose profiles and
    no-sign-request by allowing a custom credentials provider. Right now,
    only the CRT providers that we've explicitly bound can be used here, but
    eventually we'll bind the `aws_credentials_provider_delegate` version
    that can invoke an arbitrary callback to fetch credentials. For now,
    added a static provider that can be manually configured with credentials
    for customers using the client as a library with credentials vended
    from elsewhere.
    
    I also took this chance to write a test for the profile credentials
    provider. It's a little annoying to do because CLI profiles are global
    state, so we do it by forking the test into a new process where it's
    safe to futz with the environment variables we need. Along the way, I
    realized that the CRT auth provider prefers the AWS_PROFILE environment
    variable over `--profile`, which is the wrong behavior, so I fixed that
    by unsetting the variable in main.rs.
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    * PR feedback
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    * Update mountpoint-s3-crt/src/auth/credentials.rs
    
    Co-authored-by: Daniel Carl Jones <danny@danielcarl.info>
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    
    ---------
    
    Signed-off-by: James Bornholt <bornholt@amazon.com>
    Co-authored-by: Daniel Carl Jones <danny@danielcarl.info>

--- mountpoint-s3-crt/src/auth/credentials.rs
+++ mountpoint-s3-crt/src/auth/credentials.rs
@@ -1,17 +1,20 @@
 //! AWS credentials providers
 
+use std::fmt::Debug;
+use std::ptr::NonNull;
+
 use mountpoint_s3_crt_sys::{
-    aws_credentials_provider, aws_credentials_provider_chain_default_options,
+    aws_credentials_provider, aws_credentials_provider_acquire, aws_credentials_provider_chain_default_options,
     aws_credentials_provider_new_chain_default, aws_credentials_provider_new_profile,
-    aws_credentials_provider_profile_options, aws_credentials_provider_release,
+    aws_credentials_provider_new_static, aws_credentials_provider_profile_options, aws_credentials_provider_release,
+    aws_credentials_provider_static_options,
 };
 
 use crate::auth::auth_library_init;
 use crate::common::allocator::Allocator;
 use crate::common::error::Error;
 use crate::io::channel_bootstrap::ClientBootstrap;
-use crate::{CrtError as _, StringExt};
-use std::ptr::NonNull;
+use crate::{CrtError as _, StringExt as _};
 
 /// Options for creating a default credentials provider
 #[derive(Debug)]
@@ -27,6 +30,26 @@ pub struct CredentialsProviderProfileOptions<'a> {
     pub bootstrap: &'a mut ClientBootstrap,
     /// The name of profile to use.
     pub profile_name_override: &'a str,
+}
+
+/// Options for creating a static credentials provider
+pub struct CredentialsProviderStaticOptions<'a> {
+    /// AWS access key ID
+    pub access_key_id: &'a str,
+    /// AWS secret access key
+    pub secret_access_key: &'a str,
+    /// AWS session token (only required for some credentials sources, e.g. STS)
+    pub session_token: Option<&'a str>,
+}
+
+impl Debug for CredentialsProviderStaticOptions<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CredentialsProviderStaticOptions")
+            .field("access_key_id", &"** redacted **")
+            .field("secret_access_key", &"** redacted **")
+            .field("session_token", &self.session_token.map(|_| "** redacted **"))
+            .finish()
+    }
 }
 
 /// A credentials provider is an object that has an asynchronous query function for retrieving AWS
@@ -74,6 +97,39 @@ impl CredentialsProvider {
         };
 
         Ok(Self { inner })
+    }
+
+    /// Creates a static credential provider that always returns the given credentials
+    pub fn new_static(allocator: &Allocator, options: CredentialsProviderStaticOptions) -> Result<Self, Error> {
+        auth_library_init(allocator);
+
+        // SAFETY: aws_credentials_provider_new_static makes a copy of the strings
+        let inner = unsafe {
+            let inner_options = aws_credentials_provider_static_options {
+                access_key_id: options.access_key_id.as_aws_byte_cursor(),
+                secret_access_key: options.secret_access_key.as_aws_byte_cursor(),
+                session_token: options
+                    .session_token
+                    .map(|t| t.as_aws_byte_cursor())
+                    .unwrap_or_default(),
+                ..Default::default()
+            };
+
+            aws_credentials_provider_new_static(allocator.inner.as_ptr(), &inner_options).ok_or_last_error()?
+        };
+
+        Ok(Self { inner })
+    }
+}
+
+impl Clone for CredentialsProvider {
+    fn clone(&self) -> Self {
+        // SAFETY: `self.inner` is a valid `aws_credentials_provider` for as long as `self` exists
+        unsafe {
+            aws_credentials_provider_acquire(self.inner.as_ptr());
+        }
+
+        Self { inner: self.inner }
     }
 }
 
