cd908322e5469be35f9815123f33a4d1000652ab
Alessandro Passaro <alexpax@amazon.co.uk>
2023-06-06 14:18:15 +0100

    Add put_object test
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3-client/tests/put_object.rs
+++ mountpoint-s3-client/tests/put_object.rs
@@ -12,7 +12,7 @@ use rand::Rng;
 async fn test_put_object(client: &impl ObjectClient, bucket: &str, prefix: &str) {
     let mut rng = rand::thread_rng();
 
-    let key = format!("{prefix}/hello");
+    let key = format!("{prefix}hello");
 
     let mut contents = vec![0u8; 32];
     rng.fill(&mut contents[..]);
@@ -39,7 +39,7 @@ object_client_test!(test_put_object);
 async fn test_put_object_multi_part(client: &impl ObjectClient, bucket: &str, prefix: &str) {
     let mut rng = rand::thread_rng();
 
-    let key = format!("{prefix}/hello");
+    let key = format!("{prefix}hello");
 
     let mut contents = vec![0u8; 32];
     rng.fill(&mut contents[..]);
@@ -63,3 +63,35 @@ async fn test_put_object_multi_part(client: &impl ObjectClient, bucket: &str, pr
 }
 
 object_client_test!(test_put_object_multi_part);
+
+// Test for multi-part PUT interface. Splits up a large object into a number of pieces, and streams
+// the pieces to the object client. Checks contents are correct using a GET.
+async fn test_put_object_large(client: &impl ObjectClient, bucket: &str, prefix: &str) {
+    let mut rng = rand::thread_rng();
+
+    let key = format!("{prefix}hello");
+
+    const OBJECT_SIZE: usize = 32 * 1024 * 1024;
+    const CHUNK_SIZE: usize = 1024 * 1024 + 1;
+
+    let mut contents = vec![0u8; OBJECT_SIZE];
+    rng.fill(&mut contents[..]);
+
+    let mut request = client
+        .put_object(bucket, &key, &Default::default())
+        .await
+        .expect("put_object failed");
+
+    for chunk in contents.chunks(CHUNK_SIZE) {
+        request.write(chunk).await.unwrap();
+    }
+    request.complete().await.unwrap();
+
+    let result = client
+        .get_object(bucket, &key, None, None)
+        .await
+        .expect("get_object failed");
+    check_get_result(result, None, &contents[..]).await;
+}
+
+object_client_test!(test_put_object_large);
