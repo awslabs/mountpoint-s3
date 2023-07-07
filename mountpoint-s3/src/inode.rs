619a06d39e0993ebeb150e707734fc7fb9f7dccb
Alessandro Passaro <alexpax@amazon.co.uk>
2023-07-07 11:48:12 +0100

    Remove TODO
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3/src/inode.rs
+++ mountpoint-s3/src/inode.rs
@@ -400,8 +400,6 @@ impl Superblock {
             }
         };
 
-        // TODO: When inode lookup/ref counting is implemented, decrement here to eventually remove from superblock.
-
         Ok(())
     }
 }
