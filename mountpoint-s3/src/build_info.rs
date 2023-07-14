8d44b12bdd387ee9e7359f754e62d294ad267003
Daniel Carl Jones <djonesoa@amazon.com>
2023-07-14 22:31:27 +0100

    Allow omitting commit hash from version when specified as official release (#376)
    Signed-off-by: Daniel Carl Jones <djonesoa@amazon.com>

--- mountpoint-s3/src/build_info.rs
+++ mountpoint-s3/src/build_info.rs
@@ -7,10 +7,10 @@ mod built {
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
 }
 
-/// Valid SemVer version constructed using declared Cargo version and short commit hash.
+/// Valid SemVer version constructed using declared Cargo version and short commit hash if needed.
 pub const FULL_VERSION: &str = {
     // A little hacky so we can pull out the hash as a const
-    if built::GIT_COMMIT_HASH_SHORT.is_some() {
+    if built::GIT_COMMIT_HASH_SHORT.is_some() && !is_official_aws_release() {
         const COMMIT_HASH_STR: &str = match built::GIT_COMMIT_HASH_SHORT {
             Some(hash) => hash,
             // Evaluated at compile time, but never used
@@ -21,3 +21,8 @@ pub const FULL_VERSION: &str = {
         built::PKG_VERSION
     }
 };
+
+/// Checks environment to see if this build is for an official Mountpoint for Amazon S3 release.
+const fn is_official_aws_release() -> bool {
+    option_env!("MOUNTPOINT_S3_AWS_RELEASE").is_some()
+}
