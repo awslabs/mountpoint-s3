39f5eb7eb4b52a56c297427f67193ef9b87c0f68
Monthon Klongklaew <monthonk@amazon.com>
2023-10-05 16:19:16 +0000

    Refactor signing configuration
    Signed-off-by: Monthon Klongklaew <monthonk@amazon.com>

--- mountpoint-s3-crt/src/auth/signing_config.rs
+++ mountpoint-s3-crt/src/auth/signing_config.rs
@@ -1,7 +1,8 @@
 //! Configuration for signing requests to AWS APIs
 
 use crate::auth::credentials::CredentialsProvider;
-use mountpoint_s3_crt_sys::{aws_signing_algorithm, aws_signing_config_aws};
+use crate::ToAwsByteCursor;
+use mountpoint_s3_crt_sys::{aws_s3_init_default_signing_config, aws_signing_algorithm, aws_signing_config_aws};
 use std::ffi::OsString;
 use std::fmt::Debug;
 use std::marker::PhantomPinned;
@@ -18,7 +19,7 @@ pub(crate) struct SigningConfigInner {
     pub(crate) credentials_provider: CredentialsProvider,
 
     /// An owned copy of the service string, since the `aws_signing_config_aws` holds a pointer to it.
-    pub(crate) service: OsString,
+    pub(crate) service: Option<OsString>,
 
     /// This forces the struct to be !Unpin, because the signing config can contain pointers to itself
     pub(crate) _pinned: PhantomPinned,
@@ -29,6 +30,49 @@ impl Debug for SigningConfigInner {
         f.debug_struct("SigningConfigInner")
             .field("region", &self.region)
             .finish()
+    }
+}
+
+impl SigningConfigInner {
+    /// Create a new [SigningConfig] with default options.
+    pub fn new(region: &str, credentials_provider: CredentialsProvider) -> Self {
+        let mut signing_config = SigningConfigInner {
+            inner: Default::default(),
+            region: region.to_owned().into(),
+            credentials_provider,
+            service: None,
+            _pinned: Default::default(),
+        };
+
+        let credentials_provider = signing_config.credentials_provider.inner.as_ptr();
+        // SAFETY: `region` is owned by signing_config (see `region.to_owned()` above),
+        // so the byte cursors we create here will point to bytes that are valid as long as this SigningConfig is.
+        // singing_config owns `credential_provider` that is valid as long as this SingingConfig is.
+        unsafe {
+            let region_cursor = signing_config.region.as_aws_byte_cursor();
+            aws_s3_init_default_signing_config(&mut signing_config.inner, region_cursor, credentials_provider);
+        }
+
+        signing_config
+    }
+
+    /// Set the service name
+    pub fn service(&mut self, service: &str) {
+        self.service = Some(service.to_owned().into());
+        // SAFETY: `service` is owned by signing_config,
+        // so the byte cursors we create here will point to bytes that are valid as long as this SigningConfig is.
+        let service_cursor = unsafe { self.service.as_ref().unwrap().as_aws_byte_cursor() };
+        self.inner.service = service_cursor;
+    }
+
+    /// Set whether to use double URI encode or not
+    pub fn use_double_uri_encode(&mut self, use_double_uri_encode: bool) {
+        self.inner.flags.set_use_double_uri_encode(use_double_uri_encode as u32);
+    }
+
+    /// Set the signing algorithm
+    pub fn algorithm(&mut self, algorithm: SigningAlgorithm) {
+        self.inner.algorithm = algorithm.into();
     }
 }
 
