e19d34788f509cc515c4ab52f59e82a7c56d4b19
github-action-benchmark <github@users.noreply.github.com>
2023-11-28 15:53:17 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 042a710a17576879ede1bc4a42712b5b84c22600

--- dev/latency_bench/data.js
+++ dev/latency_bench/data.js
@@ -1,62 +1,8 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1701172282380,
+  "lastUpdate": 1701186797091,
   "repoUrl": "https://github.com/awslabs/mountpoint-s3",
   "entries": {
     "Benchmark": [
-      {
-        "commit": {
-          "author": {
-            "email": "bornholt@amazon.com",
-            "name": "James Bornholt",
-            "username": "jamesbornholt"
-          },
-          "committer": {
-            "email": "noreply@github.com",
-            "name": "GitHub",
-            "username": "web-flow"
-          },
-          "distinct": true,
-          "id": "06a1d7d78388344621cbc4d94f96a2883e64fad8",
-          "message": "Add instance and config metadata to user agent (#608)\n\n* Add instance and config metadata to user agent\n\nWe'd like to record the same metadata that AWS SDKs gather so we can\nbetter understand how different Mountpoint features are used. This\nchange adds support for detecting platform and instance metadata and\nincluding it in HTTP User-agents. We follow the SDK template for\nserializing this metadata.\n\nTo make this cleaner, I moved the instance info logic into the client\ncrate so that all users can get this kind of user agent. The new\n`UserAgent` struct supports addings arbitrary key/value pairs, and we\nuse that in Mountpoint to record basic configurations.\n\nUser agents are always a bit annoying to test, but I manually verified\nin a few cases (caching enabled/disabled) that this change was sending\nthe expected headers.\n\nSigned-off-by: James Bornholt <bornholt@amazon.com>\n\n* Fix typo\n\nSigned-off-by: James Bornholt <bornholt@amazon.com>\n\n---------\n\nSigned-off-by: James Bornholt <bornholt@amazon.com>",
-          "timestamp": "2023-11-16T19:20:04Z",
-          "tree_id": "a68e805c59fe657df916a6547ab12ac2c1185d73",
-          "url": "https://github.com/awslabs/mountpoint-s3/commit/06a1d7d78388344621cbc4d94f96a2883e64fad8"
-        },
-        "date": 1700164313871,
-        "tool": "customSmallerIsBetter",
-        "benches": [
-          {
-            "name": "readdir_100",
-            "value": 0.073,
-            "unit": "seconds"
-          },
-          {
-            "name": "readdir_1000",
-            "value": 0.171,
-            "unit": "seconds"
-          },
-          {
-            "name": "readdir_10000",
-            "value": 1.113,
-            "unit": "seconds"
-          },
-          {
-            "name": "readdir_100000",
-            "value": 10.476,
-            "unit": "seconds"
-          },
-          {
-            "name": "time_to_first_byte_read",
-            "value": 90.8073345,
-            "unit": "milliseconds"
-          },
-          {
-            "name": "time_to_first_byte_read_small_file",
-            "value": 56.213719,
-            "unit": "milliseconds"
-          }
-        ]
-      },
       {
         "commit": {
           "author": {
@@ -1079,6 +1025,60 @@ window.BENCHMARK_DATA = {
           {
             "name": "time_to_first_byte_read_small_file",
             "value": 53.916488799999996,
+            "unit": "milliseconds"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "monthonk@amazon.com",
+            "name": "Monthon Klongklaew",
+            "username": "monthonk"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "042a710a17576879ede1bc4a42712b5b84c22600",
+          "message": "Update CRT submodules to latest releases (#641)\n\nSigned-off-by: Monthon Klongklaew <monthonk@amazon.com>",
+          "timestamp": "2023-11-28T15:42:18Z",
+          "tree_id": "83650ef352ad65f2ab99bac688fbb7dc7ca72d53",
+          "url": "https://github.com/awslabs/mountpoint-s3/commit/042a710a17576879ede1bc4a42712b5b84c22600"
+        },
+        "date": 1701186796358,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "readdir_100",
+            "value": 0.082,
+            "unit": "seconds"
+          },
+          {
+            "name": "readdir_1000",
+            "value": 0.186,
+            "unit": "seconds"
+          },
+          {
+            "name": "readdir_10000",
+            "value": 1.1,
+            "unit": "seconds"
+          },
+          {
+            "name": "readdir_100000",
+            "value": 10.437,
+            "unit": "seconds"
+          },
+          {
+            "name": "time_to_first_byte_read",
+            "value": 80.910663,
+            "unit": "milliseconds"
+          },
+          {
+            "name": "time_to_first_byte_read_small_file",
+            "value": 79.99384429999999,
             "unit": "milliseconds"
           }
         ]
