df513faf5fd2040c9ec37112e0f0f1793258c30f
Vladislav Volodkin <vlaad@amazon.co.uk>
2024-03-05 15:49:09 +0000

    Add fio job to benchmark read-skip-read pattern
    Signed-off-by: Vladislav Volodkin <vlaad@amazon.co.uk>

--- mountpoint-s3/scripts/fs_bench.sh
+++ mountpoint-s3/scripts/fs_bench.sh
@@ -73,7 +73,8 @@ run_fio_job() {
   echo "done"
 
   # combine the results and find an average value
-  jq -n 'reduce inputs.jobs[] as $job (null; .name = $job.jobname | .len += 1 | .value += (if ($job."job options".rw == "read")
+  # TODO: requires testing
+  jq -n 'reduce inputs.jobs[] as $job (null; .name = $job.jobname | .len += 1 | .value += (if ($job."job options".rw | startswith("read"))
       then $job.read.bw / 1024
       elif ($job."job options".rw == "randread") then $job.read.bw / 1024
       elif ($job."job options".rw == "randwrite") then $job.write.bw / 1024
