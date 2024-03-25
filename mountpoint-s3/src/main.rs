360d9d25aeac264f8247ea0e3e219c4b7b0055d2
Chris Soyars <chris@union.ai>
2024-03-25 13:36:20 -0700

    Wire up prometheus exporter server

--- mountpoint-s3/src/main.rs
+++ mountpoint-s3/src/main.rs
@@ -1,3 +1,8 @@
+use metrics_exporter_prometheus::PrometheusBuilder;
+
 fn main() -> anyhow::Result<()> {
+    PrometheusBuilder::new()
+        .install()
+        .expect("fialed to install Prometheus exporter");
     mountpoint_s3::cli::main(mountpoint_s3::cli::create_s3_client)
 }
