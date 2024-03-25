c41edca6862d72b50713b8cfe70d638fd922fbc1
Chris Soyars <chris@union.ai>
2024-03-25 13:38:14 -0700

    Fix typo

--- mountpoint-s3/src/main.rs
+++ mountpoint-s3/src/main.rs
@@ -3,6 +3,6 @@ use metrics_exporter_prometheus::PrometheusBuilder;
 fn main() -> anyhow::Result<()> {
     PrometheusBuilder::new()
         .install()
-        .expect("fialed to install Prometheus exporter");
+        .expect("failed to install Prometheus exporter");
     mountpoint_s3::cli::main(mountpoint_s3::cli::create_s3_client)
 }
