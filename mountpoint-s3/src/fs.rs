e1b6eae2165fb8fcf616be987e01a1229dd2968b
Andres Santana <hernaa@amazon.com>
2024-03-26 07:10:56 +0000

    Creating a fn for creating a default handle.
    Signed-off-by: Andres Santana <hernaa@amazon.com>

--- mountpoint-s3/src/fs.rs
+++ mountpoint-s3/src/fs.rs
@@ -939,10 +939,14 @@ where
         Ok(len)
     }
 
+    async fn default_handle(&self, parent: InodeNo) -> Result<ReaddirHandle, InodeError> {
+        self.superblock.readdir(&self.client, parent, 1000).await
+    }
+
     pub async fn opendir(&self, parent: InodeNo, _flags: i32) -> Result<Opened, Error> {
         trace!("fs:opendir with parent {:?} flags {:#b}", parent, _flags);
 
-        let inode_handle = self.superblock.readdir(&self.client, parent, 1000).await?;
+        let inode_handle = self.default_handle(parent).await?;
 
         let fh = self.next_handle();
         let handle = DirHandle {
@@ -1000,7 +1004,7 @@ where
         if offset == 0 && dir_handle.offset() != 0 {
             // only do this if this is not the first call with offset 0
             dir_handle.rewind_offset();
-            let new_handle = self.superblock.readdir(&self.client, parent, 1000).await?;
+            let new_handle = self.default_handle( parent).await?;
             *dir_handle.handle.lock().await = new_handle;
         }
 
