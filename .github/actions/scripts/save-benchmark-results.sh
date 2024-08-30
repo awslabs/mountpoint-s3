0f04ea4daa4f3fa68421c69b0179d09bae044d6c
Volodkin Vladislav <vladvolodkin@gmail.com>
2024-08-30 09:13:02 +0100

    Run bench once a day, no scheduled on forks, store commit id (#992)
    Signed-off-by: Vlad Volodkin <vlaad@amazon.com>
    Co-authored-by: Vlad Volodkin <vlaad@amazon.com>

--- .github/actions/scripts/save-benchmark-results.sh
+++ .github/actions/scripts/save-benchmark-results.sh
@@ -4,10 +4,10 @@ set -e
 
 aws --version || (curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && unzip awscliv2.zip && sudo ./aws/install)
 
+echo "{\"commit\": {\"id\": \"${COMMIT_ID}\"}, \"benches\": $(cat results/output.json)}" > results/s3_output.json
 DATE=$(date +%s)
 echo "Storing benchmark results as ${S3_BENCH_RESULTS_PREFIX}/${DATE}.json"
 aws s3 cp \
     --region ${S3_BENCH_REGION} \
-    results/output.json \
-    s3://${S3_BENCH_BUCKET_NAME}/${S3_BENCH_RESULTS_PREFIX}/${DATE}.json \
-    > /dev/null 2>&1
+    results/s3_output.json \
+    s3://${S3_BENCH_BUCKET_NAME}/${S3_BENCH_RESULTS_PREFIX}/${DATE}.json
