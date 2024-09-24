fcf4fc91bf1b309b9210b47e588602f0c2da9bc7
Alessandro Passaro <alexpax@amazon.co.uk>
2024-09-24 10:19:36 +0000

    Improve error handling
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3/src/data_cache/express_data_cache.rs
+++ mountpoint-s3/src/data_cache/express_data_cache.rs
@@ -1,17 +1,19 @@
 use crate::object::ObjectId;
 
-use super::{BlockIndex, ChecksummedBytes, DataCache, DataCacheResult};
+use super::{BlockIndex, ChecksummedBytes, DataCache, DataCacheError, DataCacheResult};
 
 use async_trait::async_trait;
 use bytes::BytesMut;
 use futures::{pin_mut, StreamExt};
+use mountpoint_s3_client::error::{GetObjectError, ObjectClientError};
 use mountpoint_s3_client::types::PutObjectParams;
 use mountpoint_s3_client::{ObjectClient, PutObjectRequest};
 use sha2::{Digest, Sha256};
-use tracing::{trace, Instrument};
+use tracing::Instrument;
 
 const CACHE_VERSION: &str = "V1";
 
+/// A data cache on S3 Express One Zone that can be shared across Mountpoint instances.
 pub struct ExpressDataCache<Client: ObjectClient> {
     client: Client,
     bucket_name: String,
@@ -19,10 +21,23 @@ pub struct ExpressDataCache<Client: ObjectClient> {
     block_size: u64,
 }
 
+impl<S, C> From<ObjectClientError<S, C>> for DataCacheError
+where
+    S: std::error::Error + Send + Sync + 'static,
+    C: std::error::Error + Send + Sync + 'static,
+{
+    fn from(e: ObjectClientError<S, C>) -> Self {
+        DataCacheError::IoFailure(e.into())
+    }
+}
+
 impl<Client> ExpressDataCache<Client>
 where
     Client: ObjectClient + Send + Sync + 'static,
 {
+    /// Create a new instance.
+    ///
+    /// TODO: consider adding some validation of the bucket.
     pub fn new(bucket_name: &str, client: Client, source_description: &str, block_size: u64) -> Self {
         let prefix = hex::encode(
             Sha256::new()
@@ -49,35 +64,29 @@ where
         &self,
         cache_key: &ObjectId,
         block_idx: BlockIndex,
-        _block_offset: u64, // TODO: should we use this?
+        block_offset: u64,
     ) -> DataCacheResult<Option<ChecksummedBytes>> {
+        if block_offset != block_idx * self.block_size {
+            return Err(DataCacheError::InvalidBlockOffset);
+        }
+
         let object_key = block_key(&self.prefix, cache_key, block_idx);
         let result = match self.client.get_object(&self.bucket_name, &object_key, None, None).await {
-            Ok(ok_result) => ok_result,
-            Err(_e) => {
-                //TODO: assuming that key does not exists but it could be something else, so we will need to do something smarter here
-                return DataCacheResult::Ok(None);
-            }
+            Ok(result) => result,
+            Err(ObjectClientError::ServiceError(GetObjectError::NoSuchKey)) => return Ok(None),
+            Err(e) => return Err(e.into()),
         };
 
         pin_mut!(result);
 
+        // TODO: optimize for the common case of a single chunk.
         let mut buffer = BytesMut::default();
-        loop {
-            trace!("ExpressDataCache::get_block {}", object_key);
-            match result.next().await {
-                Some(Ok((_offset, body))) => {
-                    // TODO: check offset expectation
-                    buffer.extend_from_slice(&body);
-                }
-                Some(Err(_e)) => {
-                    // TODO: what should we do here? anything better?
-                    return DataCacheResult::Ok(None);
-                }
-                None => {
-                    break;
-                }
+        while let Some(chunk) = result.next().await {
+            let (offset, body) = chunk?;
+            if offset != buffer.len() as u64 {
+                return Err(DataCacheError::InvalidBlockOffset);
             }
+            buffer.extend_from_slice(&body);
         }
         let buffer = buffer.freeze();
         DataCacheResult::Ok(Some(buffer.into()))
@@ -87,22 +96,25 @@ where
         &self,
         cache_key: ObjectId,
         block_idx: BlockIndex,
-        _block_offset: u64,
+        block_offset: u64,
         bytes: ChecksummedBytes,
     ) -> DataCacheResult<()> {
+        if block_offset != block_idx * self.block_size {
+            return Err(DataCacheError::InvalidBlockOffset);
+        }
+
         let object_key = block_key(&self.prefix, &cache_key, block_idx);
-        trace!("ExpressDataCache::put_block {}", object_key);
-        // TODO: handle errors in a better way than just expects
+
+        // TODO: ideally we should use a simple Put rather than MPU.
         let params = PutObjectParams::new();
         let mut req = self
             .client
             .put_object(&self.bucket_name, &object_key, &params)
             .in_current_span()
-            .await
-            .expect("could not create req");
-        let (data, _crc) = bytes.into_inner().expect("could not unpack checksummed bytes");
-        req.write(&data).await.expect("unable to write");
-        req.complete().await.expect("unable to complete upload");
+            .await?;
+        let (data, _crc) = bytes.into_inner().map_err(|_| DataCacheError::InvalidBlockContent)?;
+        req.write(&data).await?;
+        req.complete().await?;
 
         DataCacheResult::Ok(())
     }
