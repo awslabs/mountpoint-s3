fdee7cf0f1c7567a5e9dbe26ba0b6bf4361bb6c4
Alessandro Passaro <alexpax@amazon.co.uk>
2024-09-24 15:35:23 +0100

    Add unit test
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3/src/data_cache/express_data_cache.rs
+++ mountpoint-s3/src/data_cache/express_data_cache.rs
@@ -133,3 +133,105 @@ fn block_key(prefix: &str, cache_key: &ObjectId, block_idx: BlockIndex) -> Strin
     );
     format!("{}/{}/{:010}", prefix, hashed_cache_key, block_idx)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::checksums::ChecksummedBytes;
+    use crate::sync::Arc;
+
+    use mountpoint_s3_client::mock_client::{MockClient, MockClientConfig};
+    use mountpoint_s3_client::types::ETag;
+
+    #[tokio::test]
+    async fn test_put_get() {
+        let data_1 = ChecksummedBytes::new("Foo".into());
+        let data_2 = ChecksummedBytes::new("Bar".into());
+        let data_3 = ChecksummedBytes::new("Baz".into());
+
+        let bucket = "test-bucket";
+        let part_size = 8 * 1024 * 1024;
+        let config = MockClientConfig {
+            bucket: bucket.to_string(),
+            part_size,
+            enable_backpressure: true,
+            initial_read_window_size: part_size,
+            ..Default::default()
+        };
+        let client = Arc::new(MockClient::new(config));
+
+        let block_size = 512 * 1024;
+        let cache = ExpressDataCache::new(bucket, client, "unique source description", block_size);
+        let cache_key_1 = ObjectId::new("a".into(), ETag::for_tests());
+        let cache_key_2 = ObjectId::new(
+            "longkey_".repeat(128), // 1024 bytes, max length for S3 keys
+            ETag::for_tests(),
+        );
+
+        let block = cache
+            .get_block(&cache_key_1, 0, 0)
+            .await
+            .expect("cache should be accessible");
+        assert!(
+            block.is_none(),
+            "no entry should be available to return but got {:?}",
+            block,
+        );
+
+        // PUT and GET, OK?
+        cache
+            .put_block(cache_key_1.clone(), 0, 0, data_1.clone())
+            .await
+            .expect("cache should be accessible");
+        let entry = cache
+            .get_block(&cache_key_1, 0, 0)
+            .await
+            .expect("cache should be accessible")
+            .expect("cache entry should be returned");
+        assert_eq!(
+            data_1, entry,
+            "cache entry returned should match original bytes after put"
+        );
+
+        // PUT AND GET block for a second key, OK?
+        cache
+            .put_block(cache_key_2.clone(), 0, 0, data_2.clone())
+            .await
+            .expect("cache should be accessible");
+        let entry = cache
+            .get_block(&cache_key_2, 0, 0)
+            .await
+            .expect("cache should be accessible")
+            .expect("cache entry should be returned");
+        assert_eq!(
+            data_2, entry,
+            "cache entry returned should match original bytes after put"
+        );
+
+        // PUT AND GET a second block in a cache entry, OK?
+        cache
+            .put_block(cache_key_1.clone(), 1, block_size, data_3.clone())
+            .await
+            .expect("cache should be accessible");
+        let entry = cache
+            .get_block(&cache_key_1, 1, block_size)
+            .await
+            .expect("cache should be accessible")
+            .expect("cache entry should be returned");
+        assert_eq!(
+            data_3, entry,
+            "cache entry returned should match original bytes after put"
+        );
+
+        // Entry 1's first block still intact
+        let entry = cache
+            .get_block(&cache_key_1, 0, 0)
+            .await
+            .expect("cache should be accessible")
+            .expect("cache entry should be returned");
+        assert_eq!(
+            data_1, entry,
+            "cache entry returned should match original bytes after put"
+        );
+    }
+}
