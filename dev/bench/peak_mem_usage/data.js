b126c67b5c4f3e2b98c796f4d4d6294f81f4a18b
github-action-benchmark <github@users.noreply.github.com>
2024-10-07 10:46:22 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 5abbce51cb47b4db9176992308cf037c2729e7be

--- dev/bench/peak_mem_usage/data.js
+++ dev/bench/peak_mem_usage/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1728058048990,
+  "lastUpdate": 1728297982490,
   "repoUrl": "https://github.com/awslabs/mountpoint-s3",
   "entries": {
     "Benchmark": [
@@ -1784,6 +1784,125 @@ window.BENCHMARK_DATA = {
           {
             "name": "seq_write",
             "value": 416.04296875,
+            "unit": "MiB"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alexpax@amazon.co.uk",
+            "name": "Alessandro Passaro",
+            "username": "passaro"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5abbce51cb47b4db9176992308cf037c2729e7be",
+          "message": "Tidy up the fs module (#1051)\n\n* Refactor fs into submodules\n\nSigned-off-by: Alessandro Passaro <alexpax@amazon.co.uk>\n\n* Reorder types in fs module\n\nSigned-off-by: Alessandro Passaro <alexpax@amazon.co.uk>\n\n---------\n\nSigned-off-by: Alessandro Passaro <alexpax@amazon.co.uk>",
+          "timestamp": "2024-10-07T08:47:24Z",
+          "tree_id": "60b95595d6b1f9df7d227e1a603d4eddc3be50b5",
+          "url": "https://github.com/awslabs/mountpoint-s3/commit/5abbce51cb47b4db9176992308cf037c2729e7be"
+        },
+        "date": 1728297982454,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "rand_read_4t_direct",
+            "value": 135.90625,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read_4t_direct_small",
+            "value": 378.41015625,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read_4t",
+            "value": 128.66015625,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read_4t_small",
+            "value": 397.46875,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read_direct",
+            "value": 82.47265625,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read_direct_small",
+            "value": 317.60546875,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read",
+            "value": 79.05859375,
+            "unit": "MiB"
+          },
+          {
+            "name": "rand_read_small",
+            "value": 326.6953125,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_4t_direct",
+            "value": 41225.51953125,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_4t_direct_small",
+            "value": 414.859375,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_4t",
+            "value": 47145.296875,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_4t_small",
+            "value": 540.94921875,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_direct",
+            "value": 9189.7265625,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_direct_small",
+            "value": 269.5390625,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read",
+            "value": 9915.28515625,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_skip_17m",
+            "value": 12742.359375,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_read_small",
+            "value": 282.875,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_write_direct",
+            "value": 506.56640625,
+            "unit": "MiB"
+          },
+          {
+            "name": "seq_write",
+            "value": 398.1015625,
             "unit": "MiB"
           }
         ]
