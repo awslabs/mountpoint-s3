a296c7e7bdfbbddeb3d96c5bfc26681ae6e1de36
Alessandro Passaro <alexpax@amazon.co.uk>
2024-10-09 10:28:25 +0100

    Tidy up
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3/src/fs/flags.rs
+++ mountpoint-s3/src/fs/flags.rs
@@ -1,34 +1,63 @@
+/// Helper to define type-safe flags from `libc` constants.
+/// Usage:
+///
+/// ```
+/// struct MyFlags(u32);
+///
+/// libc_flags! {
+///     MyFlags : u32 {
+///         A,
+///         B,
+///         C,
+///     }
+/// }    
+/// ```
+///
+/// See [`OpenFlags`] for an example.
 macro_rules! libc_flags {
-    ($name:ident {
+    (
+        $struct_name:ident : $raw_type:ty {
         $(
         $flag_name:ident
         ),*$(,)+
     }
     ) => {
-            bitflags::bitflags! {
-                impl $name : i32 {
-                    $(
-                        const $flag_name = libc::$flag_name;
-                    )*
+        bitflags::bitflags! {
+            impl $struct_name : $raw_type {
+                $(
+                    const $flag_name = libc::$flag_name;
+                )*
 
-                    const _ = !0;
-                }
+                const _ = !0;
             }
+        }
 
-            impl std::fmt::Display for $name {
-                fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-                    bitflags::parser::to_writer(self, f)
-                }
+        impl std::fmt::Display for $struct_name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                bitflags::parser::to_writer(self, f)
             }
+        }
+
+        impl std::fmt::Debug for $struct_name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}({:#x} = {})", stringify!($struct_name), self.0, self.to_string())
+            }
+        }
+
+        impl From<$raw_type> for $struct_name {
+            fn from(value: $raw_type) -> Self {
+                Self::from_bits_retain(value)
+            }
+        }
     }
 }
 
 /// Flags used in [`open`](super::S3Filesystem::open).
-#[derive(Debug, Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct OpenFlags(i32);
 
 libc_flags! {
-    OpenFlags {
+    OpenFlags : i32 {
         O_RDONLY,
         O_WRONLY,
         O_RDWR,
@@ -42,8 +71,20 @@ libc_flags! {
     }
 }
 
-impl From<i32> for OpenFlags {
-    fn from(value: i32) -> Self {
-        Self::from_bits_retain(value)
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn conversion_test() {
+        let raw = 0x9;
+        let flags: OpenFlags = raw.into();
+        assert_eq!(flags, OpenFlags::O_WRONLY | OpenFlags::O_APPEND);
+    }
+
+    #[test]
+    fn debug_test() {
+        let flags = OpenFlags::O_WRONLY | OpenFlags::O_APPEND;
+        assert_eq!(format!("{:?}", flags), "OpenFlags(0x9 = O_WRONLY | O_APPEND)");
     }
 }
