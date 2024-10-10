aac69c4458f9b5005103c06199c3b9fab247169f
Alessandro Passaro <alexpax@amazon.co.uk>
2024-10-10 15:39:12 +0100

    Tidy up comments and tests on InputStream
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3-crt/src/io/stream.rs
+++ mountpoint-s3-crt/src/io/stream.rs
@@ -6,6 +6,47 @@ use mountpoint_s3_crt_sys::*;
 use std::marker::PhantomData;
 use std::ptr::NonNull;
 
+/// Wrapper for [aws_input_stream].
+#[derive(Debug)]
+pub struct InputStream<'a> {
+    /// The inner `aws_input_stream`. Consumers should always hold the containing `InputStream<'a>` to ensure
+    /// `inner` points to a valid buffer.
+    pub(crate) inner: NonNull<aws_input_stream>,
+
+    /// Phantom data to keep the lifetimes correct, for example, if this stream is created from an
+    /// aws_byte_cursor that has some lifetime.
+    _phantom: PhantomData<&'a [u8]>,
+}
+
+impl<'a> InputStream<'a> {
+    /// Create a new [InputStream] from a slice. The slice is not copied, and so the resulting
+    /// [InputStream] cannot outlive the slice (enforced by a lifetime restriction on the [InputStream]).
+    pub fn new_from_slice(allocator: &Allocator, buffer: &'a [u8]) -> Result<Self, Error> {
+        let cursor = aws_byte_cursor {
+            len: buffer.len(),
+            ptr: buffer.as_ptr() as *mut u8,
+        };
+
+        // SAFETY: allocator is a valid aws_allocator. `Self` has a lifetime of 'a, so Rust
+        // will ensure that the return value from this function doesn't out live the buffer.
+        let inner = unsafe { aws_input_stream_new_from_cursor(allocator.inner.as_ptr(), &cursor).ok_or_last_error()? };
+
+        Ok(Self {
+            inner,
+            _phantom: Default::default(),
+        })
+    }
+}
+
+impl<'a> Drop for InputStream<'a> {
+    fn drop(&mut self) {
+        // SAFETY: self.inner is a valid `aws_input_stream`.
+        unsafe {
+            aws_input_stream_release(self.inner.as_ptr());
+        }
+    }
+}
+
 /// Status of an [InputStream].
 #[derive(Debug)]
 pub struct StreamStatus {
@@ -55,49 +96,6 @@ impl From<SeekBasis> for aws_stream_seek_basis {
         match value {
             SeekBasis::Begin => aws_stream_seek_basis::AWS_SSB_BEGIN,
             SeekBasis::End => aws_stream_seek_basis::AWS_SSB_END,
-        }
-    }
-}
-
-/// An [InputStream] is a way to read bytes. They can be obtained either from CRT functions,
-/// or by creating a new one based on a Rust type that implements the [GenericInputStream] trait.
-#[derive(Debug)]
-pub struct InputStream<'a> {
-    /// The inner aws_input_stream
-    pub(crate) inner: NonNull<aws_input_stream>,
-
-    /// Phantom data to keep the lifetimes correct, for example, if this stream is created from an
-    /// aws_byte_cursor that has some lifetime.
-    _phantom: PhantomData<&'a [u8]>,
-}
-
-impl<'a> InputStream<'a> {
-    /// Create a new [InputStream] from a slice. The slice is not copied, and so the resulting
-    /// [InputStream] cannot outlive the slice (enforced by a lifetime restriction on the [InputStream].
-    pub fn new_from_slice(allocator: &Allocator, buffer: &'a [u8]) -> Result<Self, Error> {
-        let cursor = aws_byte_cursor {
-            len: buffer.len(),
-            ptr: buffer.as_ptr() as *mut u8,
-        };
-
-        // SAFETY: allocator is a valid aws_allocator. `Self` has a lifetime of 'a, so Rust
-        // will ensure that the return value from this function doesn't out live the buffer.
-        // We need to make sure in things that consume InputStream that we don't accidentally
-        // cause it to live longer than expected. (Or just kill this function...)
-        let inner = unsafe { aws_input_stream_new_from_cursor(allocator.inner.as_ptr(), &cursor).ok_or_last_error()? };
-
-        Ok(Self {
-            inner,
-            _phantom: Default::default(),
-        })
-    }
-}
-
-impl<'a> Drop for InputStream<'a> {
-    fn drop(&mut self) {
-        // SAFETY: self.inner is a valid `aws_input_stream`.
-        unsafe {
-            aws_input_stream_release(self.inner.as_ptr());
         }
     }
 }
@@ -177,16 +175,27 @@ mod test {
         let mut buffer = vec![0u8; 40];
 
         let nread = stream.read(&mut buffer).expect("read failed");
-
         assert_eq!(nread, bytes.len());
         assert_eq!(&buffer[..nread], &bytes[..]);
 
         let status = stream.get_status().expect("get_status failed");
-
         assert!(status.is_end_of_stream);
 
         let length = stream.get_length().expect("get_length failed");
-
         assert_eq!(length, bytes.len());
+
+        // Partial reads
+        let mut small_buffer = vec![0u8; 5];
+        stream.seek(0, SeekBasis::Begin).expect("seek to the start failed");
+
+        let nread = stream.read(&mut small_buffer).expect("read prefix failed");
+        assert_eq!(nread, 5);
+        assert_eq!(&small_buffer[..], &bytes[..5]);
+
+        stream.seek(-5, SeekBasis::End).expect("seek -5 from the end failed");
+
+        let nread = stream.read(&mut small_buffer).expect("read suffix failed");
+        assert_eq!(nread, 5);
+        assert_eq!(&small_buffer[..], &bytes[(length - 5)..]);
     }
 }
