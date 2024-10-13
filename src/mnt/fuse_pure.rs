0554c953774c5a1d204df6eb4d81c93df1ee1fd9
Christopher Berner <me@cberner.com>
2024-10-13 16:48:04 -0700

    Fix compilation on FreeBSD

--- src/mnt/fuse_pure.rs
+++ src/mnt/fuse_pure.rs
@@ -173,7 +173,14 @@ fn receive_fusermount_message(socket: &UnixStream) -> Result<File, Error> {
         message.msg_controllen = cmsg_buffer.len() as u32;
         message.msg_flags = 0;
     }
-    #[cfg(target_os = "macos")]
+    #[cfg(any(
+        target_os = "macos",
+        target_os = "freebsd",
+        target_os = "dragonfly",
+        target_os = "openbsd",
+        target_os = "bitrig",
+        target_os = "netbsd"
+    ))]
     {
         message = libc::msghdr {
             msg_name: ptr::null_mut(),
