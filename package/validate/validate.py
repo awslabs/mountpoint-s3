1a6189a8661e958dd139d21a81a25265bf7d81e2
Vlad Volodkin <vlaad@amazon.com>
2025-02-20 13:38:45 +0000

    Add validate script for SUSE
    Signed-off-by: Vlad Volodkin <vlaad@amazon.com>

--- package/validate/validate.py
+++ package/validate/validate.py
@@ -15,11 +15,13 @@ def validate(args: argparse.Namespace) -> str:
 
     package=f"{args.artifact}-{args.os}"
     if package == "deb-ubuntu":
-        image = "ubuntu/ubuntu:20.04"
+        image = "public.ecr.aws/ubuntu/ubuntu:20.04"
     elif package == "rpm-al2" or package == "gzip-al2":
-        image = "amazonlinux/amazonlinux:2"
+        image = "public.ecr.aws/amazonlinux/amazonlinux:2"
+    elif package == "rpm-sles":
+        image = "registry.suse.com/bci/bci-base:15.6"
     else:
-        raise Exception(f"unsupported OS {args.os} for {args.artifact}. Supported combinations are: deb-ubuntu, rpm-al2, gzip-al2")
+        raise Exception(f"unsupported OS {args.os} for {args.artifact}. Supported combinations are: deb-ubuntu, rpm-al2, gzip-al2, rpm-sles")
 
     print("Validating Mountpoint Release Package")
     print(f"\tVersion: {args.version}")
@@ -29,22 +31,21 @@ def validate(args: argparse.Namespace) -> str:
     print(f"\tBucket: {args.bucket}")
     print("\n")
 
-    full_image = f"public.ecr.aws/{image}"
     validate_script = f"validate-{package}.sh"
     scripts_dir = os.path.dirname(os.path.realpath(__file__))
 
-    subprocess.run(["docker", "pull", full_image])
-    subprocess.run(["docker", 
+    subprocess.run(["docker", "pull", image])
+    subprocess.run(["docker",
                     "run",
-                    "--rm", 
-                    "--cap-add=SYS_ADMIN", 
-                    "--device=/dev/fuse", 
-                    f"-v={scripts_dir}:/scripts", 
-                    f"--env=ARCH={args.arch}", 
-                    f"--env=VERSION={args.version}", 
+                    "--rm",
+                    "--cap-add=SYS_ADMIN",
+                    "--device=/dev/fuse",
+                    f"-v={scripts_dir}:/scripts",
+                    f"--env=ARCH={args.arch}",
+                    f"--env=VERSION={args.version}",
                     f"--env=BUCKET={args.bucket}",
-                    full_image, 
-                    "/bin/bash", 
+                    image,
+                    "/bin/bash",
                     f"/scripts/{validate_script}"])
 
 if __name__ == "__main__":
@@ -52,7 +53,7 @@ if __name__ == "__main__":
     p.add_argument("--version", help="the version number for the Mountpoint release", required=True)
     p.add_argument("--arch", help="the architecture to validate", required=True, choices=["x86_64", "arm64"])
     p.add_argument("--artifact", help="the artifact to validate", required=True, choices=["deb", "rpm", "gzip"])
-    p.add_argument("--os", help="the OS to validate on", required=True, choices=["ubuntu", "al2"])
+    p.add_argument("--os", help="the OS to validate on", required=True, choices=["ubuntu", "al2", "sles"])
     p.add_argument("--bucket", help="the public bucket to mount", required=True)
 
     args = p.parse_args()
