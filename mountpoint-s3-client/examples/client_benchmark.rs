4ef0f02d6010b6220e998519272adb26c83d0fbe
Daniel Carl Jones <djonesoa@amazon.com>
2025-02-25 06:06:55 +0000

    Add throughput target to all layers of benchmarks
    Signed-off-by: Daniel Carl Jones <djonesoa@amazon.com>

--- mountpoint-s3-client/examples/client_benchmark.rs
+++ mountpoint-s3-client/examples/client_benchmark.rs
@@ -101,7 +101,12 @@ enum Client {
 struct CliArgs {
     #[command(subcommand)]
     client: Client,
-    #[arg(long, help = "Desired throughput in Gbps", default_value = "10.0")]
+    #[arg(
+        long,
+        help = "Desired throughput in Gbps",
+        default_value_t = 10.0,
+        visible_alias = "maximum-throughput-gbps"
+    )]
     throughput_target_gbps: f64,
     #[arg(long, help = "Part size for multi-part GET", default_value = "8388608")]
     part_size: usize,
