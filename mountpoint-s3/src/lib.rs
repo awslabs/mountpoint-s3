8fa6314142e761db3c68e202d85417efed2e5013
Simon Beal <simobeal@amazon.com>
2025-05-16 15:44:44 +0000

    Add documentation to common.sh Add more comments to 'option_allowed' with more reasoning about nodev and nosuid Prevent printing fstab logging info more than once
    Signed-off-by: Simon Beal <simobeal@amazon.com>

--- mountpoint-s3/src/lib.rs
+++ mountpoint-s3/src/lib.rs
@@ -10,11 +10,13 @@ pub use cli::CliArgs;
 pub use fstab::FsTabCliArgs;
 pub use run::{create_s3_client, run};
 
-pub fn parse_cli_args() -> CliArgs {
+pub fn parse_cli_args(log_fstab: bool) -> CliArgs {
     let is_fstab = env::args_os().len() == 5 && env::args_os().nth(3).as_deref() == Some("-o".as_ref());
 
     let cli_args = if is_fstab && cfg!(feature = "fstab") {
-        println!("Using 'fstab' style options as detected use of `-o` argument.");
+        if log_fstab {
+            println!("Using 'fstab' style options as detected use of `-o` argument.");
+        }
         FsTabCliArgs::try_parse().and_then(|args| args.try_into())
     } else {
         CliArgs::try_parse()
--- mountpoint-s3/src/main.rs
+++ mountpoint-s3/src/main.rs
@@ -1,6 +1,6 @@
 use mountpoint_s3::{create_s3_client, parse_cli_args};
 
 fn main() -> anyhow::Result<()> {
-    let cli_args = parse_cli_args();
+    let cli_args = parse_cli_args(true);
     mountpoint_s3::run(create_s3_client, cli_args)
 }
