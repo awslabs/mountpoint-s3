46f6db41bd261670267fdf6f33a03d9e1ec67d38
Simon Beal <5381483+muddyfish@users.noreply.github.com>
2025-05-20 16:31:26 +0100

    Add fstab to GitHub CI (#1419)
    Add integration tests to Github CI
    
    Tests passed here:
    https://github.com/awslabs/mountpoint-s3/actions/runs/15135395318/job/42545885483
    
    
    ### Does this change impact existing behavior?
    - fstab now forces runs in the background instead of the foreground
    - When ran through fstab, Mountpoint now ignores the `nodev` and
    `nosuid` options, as we default to these capabilities.
    
    No breaking changes, as fstab is not released.
    
    ### Does this change need a changelog entry? Does it require a version
    change?
    
    No
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and I agree to the terms of
    the [Developer Certificate of Origin
    (DCO)](https://developercertificate.org/).
    
    ---------
    
    Signed-off-by: Simon Beal <simobeal@amazon.com>

--- mountpoint-s3/src/fstab.rs
+++ mountpoint-s3/src/fstab.rs
@@ -24,7 +24,7 @@ impl TryFrom<FsTabCliArgs> for CliArgs {
         let cli_arg_list = fstab_cli_args.into_cli_arg_list()?;
 
         let mut cli_args = CliArgs::try_parse_from(cli_arg_list)?;
-        cli_args.foreground = true;
+        cli_args.foreground = false;
         cli_args.is_fstab = true;
         Ok(cli_args)
     }
@@ -56,8 +56,15 @@ impl FsTabCliArgs {
     /// The hard-coded options listed aren't relevant to Mountpoint, but means we can't add them in the future.
     /// Options prefixed with `x-` are 'comments' in fstab, and can be ignored by us.
     fn option_allowed(option: &str) -> bool {
-        // Same as from https://github.com/libfuse/sshfs/blob/ed0825440c48895b7e20cc1440bbafd8d9c88eb8/sshfs.c#L533-L538 with addition of `rw` argument
-        !(["auto", "noauto", "user", "nouser", "users", "_netdev", "rw"].contains(&option) || option.starts_with("x-"))
+        // "auto" - "_netdev" from https://github.com/libfuse/sshfs/blob/ed0825440c48895b7e20cc1440bbafd8d9c88eb8/sshfs.c#L533-L538
+        // "rw" can be automatically added by systemd, which is our default, so we ignore it
+        // "nodev" and "nosuid" are the default behaviour for Mountpoint, and aren't normally allowed as CLI arguments.
+        // Ignore them to allow users to add them as arguments to avoid systemd's default behaviour of passing in "dev" and "suid"
+        !([
+            "auto", "noauto", "user", "nouser", "users", "_netdev", "rw", "nodev", "nosuid",
+        ]
+        .contains(&option)
+            || option.starts_with("x-"))
     }
 
     fn rename_option(option: String) -> String {
@@ -207,7 +214,7 @@ mod tests {
         let fstab_cli_args = FsTabCliArgs::try_parse_from(args).unwrap();
         let cli_args: CliArgs = fstab_cli_args.try_into().unwrap();
 
-        assert!(cli_args.foreground);
+        assert!(!cli_args.foreground);
         assert_eq!(cli_args.mount_point.to_str(), Some("/mnt/test"));
         assert_eq!(cli_args.bucket_name, "demo_s3_bucket");
         assert_eq!(cli_args.prefix.unwrap().as_str(), "foo/bar,baz/");
