8fc5a1d7325a52f54985a85d9a3e2ac6c71466e5
Alessandro Passaro <alexpax@amazon.co.uk>
2025-07-08 11:51:33 +0100

    Extract meta request type when reserving buffers
    Signed-off-by: Alessandro Passaro <alexpax@amazon.co.uk>

--- mountpoint-s3-client/tests/common/memory_pool.rs
+++ mountpoint-s3-client/tests/common/memory_pool.rs
@@ -1,4 +1,4 @@
-use mountpoint_s3_client::config::MemoryPool;
+use mountpoint_s3_client::config::{MemoryPool, MetaRequestType};
 
 /// Creates a memory pool to use in tests.
 pub fn new_for_tests() -> impl MemoryPool {
@@ -12,7 +12,7 @@ struct NoReusePool();
 impl MemoryPool for NoReusePool {
     type Buffer = Box<[u8]>;
 
-    fn get_buffer(&self, size: usize) -> Self::Buffer {
+    fn get_buffer(&self, size: usize, _type: MetaRequestType) -> Self::Buffer {
         vec![0u8; size].into_boxed_slice()
     }
 
