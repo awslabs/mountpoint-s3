b957bbd0764a71c71d440cf18f02d8c5ebdaba8e
Sahitya Damera <sahityad@amazon.com>
2025-07-21 11:47:00 +0000

    Remove redundant try/catch within specific benchmarks
    Signed-off-by: Sahitya Damera <sahityad@amazon.com>

--- benchmark/benchmarks/client_benchmark.py
+++ benchmark/benchmarks/client_benchmark.py
@@ -22,61 +22,57 @@ class ClientBenchmark(BaseBenchmark):
         return self.metadata
 
     def run_benchmark(self) -> Dict[str, Any]:
-        try:
-            subprocess_args = [
-                "cargo",
-                "run",
-                "--release",
-                "--example",
-                "client_benchmark",
-                "--",
-            ]
+        subprocess_args = [
+            "cargo",
+            "run",
+            "--release",
+            "--example",
+            "client_benchmark",
+            "--",
+        ]
 
-            if self.backpressure:
-                subprocess_args.append("--enable-backpressure")
-                if (initial_window_size := self.client_config['read_window_size']) is not None:
-                    subprocess_args.extend(["--initial-window-size", str(initial_window_size)])
+        if self.backpressure:
+            subprocess_args.append("--enable-backpressure")
+            if (initial_window_size := self.client_config['read_window_size']) is not None:
+                subprocess_args.extend(["--initial-window-size", str(initial_window_size)])
 
-            if (run_time := self.common_config['run_time']) is not None:
-                subprocess_args.extend(["--max-duration", f"{run_time}"])
+        if (run_time := self.common_config['run_time']) is not None:
+            subprocess_args.extend(["--max-duration", f"{run_time}"])
 
-            if (max_throughput := self.common_config.get('max_throughput_gbps')) is not None:
-                subprocess_args.extend(["--throughput-target-gbps", str(max_throughput)])
+        if (max_throughput := self.common_config.get('max_throughput_gbps')) is not None:
+            subprocess_args.extend(["--throughput-target-gbps", str(max_throughput)])
 
-            if (read_part_size := self.common_config['read_part_size']) is not None:
-                subprocess_args.extend(["--part-size", read_part_size])
+        if (read_part_size := self.common_config['read_part_size']) is not None:
+            subprocess_args.extend(["--part-size", read_part_size])
 
-            if (crt_mem_limit_gib := self.common_config.get('crt_mem_limit_gib')) is not None:
-                subprocess_args.extend(["--crt-memory-limit-gb", crt_mem_limit_gib])
+        if (crt_mem_limit_gib := self.common_config.get('crt_mem_limit_gib')) is not None:
+            subprocess_args.extend(["--crt-memory-limit-gb", crt_mem_limit_gib])
 
-            subprocess_args.extend(["--output-file", "client-output.json"])
-            subprocess_args.append("real")
-            region = self.common_config['region']
-            subprocess_args.extend(["--region", region])
+        subprocess_args.extend(["--output-file", "client-output.json"])
+        subprocess_args.append("real")
+        region = self.common_config['region']
+        subprocess_args.extend(["--region", region])
 
-            for interface in self.common_config['network_interfaces']:
-                subprocess_args.extend(["--bind", interface])
+        for interface in self.common_config['network_interfaces']:
+            subprocess_args.extend(["--bind", interface])
 
-            subprocess_args.append(self.common_config['s3_bucket'])
+        subprocess_args.append(self.common_config['s3_bucket'])
 
-            objects = self.common_config['s3_keys']
-            app_workers = self.common_config['application_workers']
-            object_size_in_gib = self.common_config['object_size_in_gib']
-            if not objects:
-                objects = self.config_parser.default_object_keys(app_workers, object_size_in_gib)
+        objects = self.common_config['s3_keys']
+        app_workers = self.common_config['application_workers']
+        object_size_in_gib = self.common_config['object_size_in_gib']
+        if not objects:
+            objects = self.config_parser.default_object_keys(app_workers, object_size_in_gib)
 
-            if len(objects) >= app_workers:
-                for obj in objects:
-                    subprocess_args.append(obj)
-            else:
-                raise ValueError("Seeing fewer objects than app workers. So cannot proceed with the run.")
+        if len(objects) >= app_workers:
+            for obj in objects:
+                subprocess_args.append(obj)
+        else:
+            raise ValueError("Seeing fewer objects than app workers. So cannot proceed with the run.")
 
-            log.info("Running client benchmark with args: %s", subprocess_args)
-            subprocess.run(subprocess_args, check=True, capture_output=True, text=True)
-            log.info("Client benchmark completed successfully.")
-        except Exception as e:
-            log.error(f"Error running Client benchmark: {e}")
-            raise RuntimeError("Client benchmark failed") from e
+        log.info("Running client benchmark with args: %s", subprocess_args)
+        subprocess.run(subprocess_args, check=True, capture_output=True, text=True)
+        log.info("Client benchmark completed successfully.")
 
     def post_process(self) -> Dict[str, Any]:
         return self.metadata
