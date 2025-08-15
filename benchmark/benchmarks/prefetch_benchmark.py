a21e11eb58696febd23f7285d270abe8e55beddc
Christian Hagemeier <chagem@amazon.com>
2025-08-15 16:05:07 +0100

    Remove left-over parameter (#1578)
    Removes accidentially forgotten parameter, fixing the prefetcher and
    client benchmarks to be executable again.
    
    
    ---
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license and I agree to the terms of
    the [Developer Certificate of Origin
    (DCO)](https://developercertificate.org/).
    
    Signed-off-by: Christian Hagemeier <chagem@amazon.com>

--- benchmark/benchmarks/prefetch_benchmark.py
+++ benchmark/benchmarks/prefetch_benchmark.py
@@ -78,7 +78,7 @@ class PrefetchBenchmark(BaseBenchmark):
 
         log.info("Prefetch benchmark command prepared with args: %s", subprocess_args)
 
-        return Command(args=subprocess_args, env=prefetch_env, capture_output=True)
+        return Command(args=subprocess_args, env=prefetch_env)
 
     def post_process(self, result: CommandResult) -> Dict[str, Any]:
         if result.returncode != 0:
