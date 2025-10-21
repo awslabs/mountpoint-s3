5702277c2241b6bf7c0d9c938b8b56a4418421b1
Daniel Carl Jones <djonesoa@amazon.com>
2025-10-21 16:13:40 +0100

    Revert "Update CRT submodules to latest releases (#1659)"
    This reverts commit 280c7e055580b596cedb1c0986899d5a560e1fd4.
    
    Signed-off-by: Daniel Carl Jones <djonesoa@amazon.com>

--- mountpoint-s3-crt-sys/build.rs
+++ mountpoint-s3-crt-sys/build.rs
@@ -253,7 +253,6 @@ fn compile_crt(output_dir: &Path) -> PathBuf {
             builder.define("DISABLE_PERL", "ON");
             builder.define("DISABLE_GO", "ON");
             builder.define("BUILD_TOOL", "OFF");
-            builder.define("ENABLE_SOURCE_MODIFICATION", "OFF");
         }
 
         // Force compiler optimizations for aws-checksums even in debug builds to improve throughput
