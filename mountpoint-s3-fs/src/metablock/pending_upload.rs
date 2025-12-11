d09d99961d42fc5b216c9ae6bf9fc9e48a40b154
Mansi Pandey <mansipnd@amazon.co.uk>
2025-12-11 19:40:16 +0000

    More refactoring/cleanup/code comments

--- mountpoint-s3-fs/src/metablock/pending_upload.rs
+++ mountpoint-s3-fs/src/metablock/pending_upload.rs
@@ -6,7 +6,8 @@ use crate::sync::{Arc, AsyncMutex};
 
 use super::{InodeError, Lookup, Metablock};
 
-/// A hook that ensures any local state and data buffered for a file handle pending upload to S3
+/// A hook that ensures any local state and data buffered for a file handle is uploaded to S3 before
+/// the handle information is cleaned up or a newer handle is being opened to override this one.
 ///
 /// ### Lifecycle
 /// 1. Created during `flush_writer` when a file handle is marked flushed
@@ -15,13 +16,6 @@ use super::{InodeError, Lookup, Metablock};
 ///    - A subsequent `release` call
 ///    - A new `open` that needs to override the flushed handle
 /// 4. Once triggered, subsequent `wait_for_completion`s return the result of the attempted upload (once available).
-///
-/// Multiple concurrent `wait_for_completion()` calls are safe as the underlying future is protected
-/// by the hook's state lock, thus executed only once, with the result cached in the
-/// `PendingUploadHookState`. The execution calls `UploadState::complete_if_in_progress` and takes
-/// a lock on the file handle state in order to do this.
-///
-/// The hook is cleaned up from the inode state when the upload is completed (in `metablock::finish_writing`)
 #[derive(Clone, Debug)]
 pub struct PendingUploadHook {
     state: Arc<AsyncMutex<PendingUploadHookState>>,
@@ -64,6 +58,14 @@ impl PendingUploadHook {
         Self { state }
     }
 
+    /// Wait for the pending upload to S3 to complete. If it hasn't started yet, start the upload.
+    ///
+    /// Multiple concurrent `wait_for_completion()` calls are safe as the underlying future is
+    /// protected by the hook's state lock, thus executed only once, with the result cached in the
+    /// `PendingUploadHookState`. The execution calls `UploadState::complete_if_in_progress` and
+    /// takes a lock on the file handle state in order to do this.
+    ///
+    /// The hook is cleaned up from the inode state when the upload is completed (in `metablock::finish_writing`)
     pub async fn wait_for_completion(&self) -> UploadCompletionResult {
         let mut state = self.state.lock().await;
         if let Some(result) = &state.result {
