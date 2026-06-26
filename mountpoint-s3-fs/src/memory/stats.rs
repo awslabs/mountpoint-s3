e0226ad1ef32a26f7edd2f3a41ac5f2e84ffba94
Renan Magagnin <renanmag@amazon.co.uk>
2026-06-26 14:04:25 +0000

    Label allocated_pages by buffer_size and buffer_count
    Signed-off-by: Renan Magagnin <renanmag@amazon.co.uk>

--- mountpoint-s3-fs/src/memory/stats.rs
+++ mountpoint-s3-fs/src/memory/stats.rs
@@ -34,19 +34,24 @@ impl SizePoolStats {
     }
 
     pub(super) fn add_empty_page(&self) {
-        metrics::gauge!("pool.empty_pages", "size" => format!("{}", self.buffer_size)).increment(1.0);
+        metrics::gauge!("pool.empty_pages", "buffer_size" => format!("{}", self.buffer_size)).increment(1.0);
         self.empty_pages.fetch_add(1, Ordering::SeqCst);
     }
 
     pub(super) fn remove_empty_page(&self) {
-        metrics::gauge!("pool.empty_pages", "size" => format!("{}", self.buffer_size)).decrement(1.0);
+        metrics::gauge!("pool.empty_pages", "buffer_size" => format!("{}", self.buffer_size)).decrement(1.0);
         self.empty_pages.fetch_sub(1, Ordering::SeqCst);
     }
 
     pub(super) fn try_allocate_page(&self, buffer_count: usize) -> Option<ManagedBuffer> {
         let size = self.buffer_size * buffer_count;
         let result = self.limiter.try_allocate(size, None, false)?;
-        metrics::gauge!("pool.allocated_pages", "size" => format!("{}", self.buffer_size)).increment(1.0);
+        metrics::gauge!(
+            "pool.allocated_pages",
+            "buffer_size" => format!("{}", self.buffer_size),
+            "buffer_count" => format!("{}", buffer_count),
+        )
+        .increment(1.0);
         self.add_empty_page();
         Some(result)
     }
